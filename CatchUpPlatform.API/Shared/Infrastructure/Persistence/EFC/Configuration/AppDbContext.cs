6668cd07ca4dd00500934dbf53b1ab0e031b5ee6
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-17 16:27:41 -0500

    refactor: remove unused set table name.

--- CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ CatchUpPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -16,8 +16,7 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-
-        builder.Entity<FavoriteSource>().ToTable("FavoriteSources");
+        
         builder.Entity<FavoriteSource>().HasKey(f => f.Id);
         builder.Entity<FavoriteSource>().Property(f => f.Id)
             .IsRequired().ValueGeneratedOnAdd();
